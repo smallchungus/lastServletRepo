83a8208cb56646cef2af7d227bca99d8de0bb6db
smallchungus <wchen1396@gmail.com>
2022-11-10 15:16:22 -0500

    so the dashboard works then I need to work out the api.

--- lastServlet/src/main/webapp/EmployeeLoginSuccessful.jsp
+++ lastServlet/src/main/webapp/EmployeeLoginSuccessful.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+<%-- <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%
 Cookie cookie = null;
@@ -22,10 +22,10 @@ Cookie cookie = null;
 				<!-- <li><input type="button" value="register" onclick="window.location='RegisterEmployee.jsp'"></li>  -->
 			</form>
 		</div>
-	</div>
+	</div> --%>
 
 
-	<table align="center" cellpadding="1" cellspacing="1" border="1"
+	<%-- 	<table align="center" cellpadding="1" cellspacing="1" border="1"
 		bgcolor="#728DCF">
 		<tr bgcolor="#000000">
 			<%
@@ -43,47 +43,46 @@ Cookie cookie = null;
 			<td><%=cookieValue%></td>
 		</tr>
 		<%
-		}
-		%>
-	</table>
-	<! -- WebDataRocks -->
-	<link href="https://cdn.webdatarocks.com/latest/webdatarocks.min.css"
-		rel="stylesheet" />
-	<script
-		src="https://cdn.webdatarocks.com/latest/webdatarocks.toolbar.min.js"></script>
-	<script src="https://cdn.webdatarocks.com/latest/webdatarocks.js"></script>
-	<! -- FusionCharts -->
-	<script
-		src="https://static.fusioncharts.com/code/latest/fusioncharts.js"></script>
-	<script
-		src="https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.fusion.js"></script>
-	<! -- WebDataRocks connector for FusionCharts -->
-	<script
-		src="https://cdn.webdatarocks.com/latest/webdatarocks.fusioncharts.js"></script>
-	<table style-="width: 100%;height: 400px">
-		<tr style="width: 50%; height: 400px">
-			<td style>
-				<div id="wdr-component"></div>
-			</td>
-			<td style>
-				<div id="fusionChartArea"></div>
-			</td>
-			<td></td>
-		</tr>
-		<tr>
-			<td style="width: 50%;">
-				<div id="fusionCharPie" style="height: 200px;"></div>
-			</td>
-			<td style="width: 50%;">
-				<div id="fusionChartBar" style="height: 200px;"></div>
-			</td>
-		</tr>
-	</table>
-	<script src="wbrFusion.js"></script>
-	
+		} --%>
+	<!-- %> -->
+	<!-- 	</table> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Semi-annual report</title>
+</head>
+<body style="background-color:#dcdaf0;">
+<h1 style="font-family:verdana;color:#6a578a;text-align:center;font-size:300%">Dashboard Reporting</h1>
 
-
-
-
+<! -- WebDataRocks -->
+<link href="https://cdn.webdatarocks.com/latest/webdatarocks.min.css" rel="stylesheet"/>
+<script src="https://cdn.webdatarocks.com/latest/webdatarocks.toolbar.min.js"></script>
+<script src="https://cdn.webdatarocks.com/latest/webdatarocks.js"></script>
+<! -- FusionCharts -->
+<script src="https://static.fusioncharts.com/code/latest/fusioncharts.js"></script>
+<script src="https://static.fusioncharts.com/code/latest/themes/fusioncharts.theme.fusion.js"></script>
+<! -- WebDataRocks connector for FusionCharts -->
+<script src="https://cdn.webdatarocks.com/latest/webdatarocks.fusioncharts.js"></script>
+<table style="width: 100%;height: 400px">
+    <tr style="width: 50%; height: 400px">
+        <td style>
+            <div id="wdr-component"></div>
+        </td>
+        <td style>
+            <div id="fusionchartArea"></div>
+        </td>
+        <td></td>
+    </tr>
+    <tr>
+        <td style="width: 50%;">
+            <div id="fusionchartPie" style="height:200px;"></div>
+        </td>
+        <td style="width: 50%;">
+            <div id="fusionchartBar" style="height:200px;"></div>
+        </td>
+    </tr>
+</table>
+<script src="wbrFusion.js"></script>
 </body>
 </html>
--- lastServlet/src/main/webapp/wbrFusion.js
+++ lastServlet/src/main/webapp/wbrFusion.js
@@ -1,3 +1,199 @@
-/**
- * 
- */
+var pivot = new WebDataRocks({
+    container: "#wdr-component",
+    toolbar: false,
+    "height": 400,
+    report: {
+        "dataSource": {
+            "dataSourceType": "json",
+            "data": getJSONData()
+        },
+        "slice": {
+             "rows": [{
+                 "uniqueName": "city"
+             },
+             {
+            "uniqueName": "Measures"
+            }],
+            "columns": [{
+                "uniqueName": "month"
+            }],
+            "measures": [{
+                "uniqueName": "price",
+                "aggregation": "sum"
+            }]
+        }
+    },
+    reportcomplete: function() {
+        pivot.off("reportcomplete");
+        createAreaChart();
+        createBarChart();
+        createPieChart();
+    }
+});
+
+function createAreaChart() {
+    var chart = new FusionCharts({
+        type: "area2d",
+        renderAt: "fusionchartArea",
+        width: "100%",
+        height: 400
+    });
+    pivot.fusioncharts.getData({
+    type: chart.chartType(), "slice": {
+        "rows": [
+            {
+                "uniqueName": "[Measures]"
+            }
+        ],
+        "columns": [
+            {
+                "uniqueName": "month"
+            }
+        ],
+        "measures": [
+            {
+                "uniqueName": "price",
+                "aggregation": "sum"
+            }
+        ]
+    }
+	}, function(data) {
+        chart.setJSONData(data);
+        chart.setChartAttribute("theme", "fusion");
+        chart.setChartAttribute("caption", "Total Revenue by Month");
+        chart.render();
+    });
+}
+
+function createBarChart() {
+    var chart = new FusionCharts({
+        type: "bar2d",
+        renderAt: "fusionchartBar",
+        width: "100%",
+        height: 400
+    });
+    pivot.fusioncharts.getData({
+    type: chart.chartType(), "slice": {
+        "rows": [
+            {
+                "uniqueName": "[Measures]"
+            }
+        ],
+        "columns": [
+            {
+                "uniqueName": "model"
+            }
+        ],
+        "measures": [
+            {
+                "uniqueName": "price",
+                "aggregation": "sum"
+            }
+        ]
+    }
+	},
+	function(data) {
+        chart.setJSONData(data);
+        chart.setChartAttribute("theme", "fusion");
+        chart.setChartAttribute("caption", "Total Revenue for Each Model");
+        chart.setChartAttribute("paletteColors", "#bc5cdb, #493999, #8790a8");
+        chart.render();
+    }
+
+    );
+}
+
+function createPieChart() {
+    var chart = new FusionCharts({
+        type: "pie2d",
+        renderAt: "fusionchartPie",
+        width: "100%",
+        height: 400
+    });
+    pivot.fusioncharts.getData({
+        type: chart.chartType()
+        }, function(data) {
+        chart.setJSONData(data);
+        chart.setChartAttribute("theme", "fusion");
+        chart.setChartAttribute("caption", "Total Revenue by City");
+        chart.setChartAttribute("paletteColors", "#9d87f5, #faa27f, #9afa7f, #e37ffa, #7de1fa, #f8fa7f");
+        chart.render();
+    });
+}
+
+function getJSONData() {
+    return [
+             {
+               "month": "January",
+               "model": "11 Pro Max",
+               "city": "Los Angeles",
+               "price": 1000
+             },
+             {
+               "month": "February",
+               "model": "XR",
+               "city": "Los Angeles",
+               "price": 700
+             },
+             {
+               "month": "March",
+               "model": "12 Pro Max",
+               "city": "Los Angeles",
+               "price": 1100
+             },
+             {
+               "month": "April",
+               "model": "11",
+               "city": "Los Angeles",
+               "price": 1000
+             },
+                          {
+               "month": "June",
+               "model": "12 Pro Max",
+               "city": "San Francisco",
+               "price": 1400
+             },
+             {
+               "month": "February",
+               "model": "11",
+               "city": "San Francisco",
+               "price": 800
+             },
+             {
+               "month": "June",
+               "model": "13 Pro",
+               "city": "San Francisco",
+               "price": 1100
+             },
+             {
+               "month": "April",
+               "model": "11 Pro",
+               "city": "San Francisco",
+               "price": 1000
+             },
+             {
+               "month": "April",
+               "model": "12 Pro",
+               "city": "San Francisco",
+               "price": 800
+             },
+             {
+               "month": "February",
+               "model": "12",
+               "city": "San Francisco",
+               "price": 500
+             },
+             {
+               "month": "April",
+               "model": "11 Pro",
+               "city": "San Francisco",
+               "price": 1400
+             },
+             {
+               "month": "April",
+               "model": "8",
+               "city": "San Francisco",
+               "price": 600
+             },
+             ]}
+             
